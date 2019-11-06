8098a49b4c951b63047fb0e732e4fb0e8ccddbdc
VyronLee <lwz_jz@hotmail.com>
2019-11-06 15:54:51 +0800

    TSTime修改为非静态类型

--- vFrame.Lockstep.Core/TSTime.cs
+++ vFrame.Lockstep.Core/TSTime.cs
@@ -2,32 +2,32 @@
 
 namespace vFrame.Lockstep.Core
 {
-    public static class TSTime
+    public class TSTime
     {
-        private static DateTime _initTime;
-        private static DateTime lastFrameTime;
+        private DateTime _initTime;
+        private DateTime lastFrameTime;
 
         /// The total number of frames that have passed (Read Only).
-        public static int frameCount { get; private set; }
+        public int frameCount { get; private set; }
 
         /// The time in seconds it took to complete the last frame (Read Only).
-        public static FixedPoint deltaTime { get; private set; }
+        public FixedPoint deltaTime { get; private set; }
 
         /// The time this frame has started (Read Only). This is the time in seconds since the last level has been loaded.
-        public static FixedPoint timeSinceLevelLoad { get; private set; }
+        public FixedPoint timeSinceLevelLoad { get; private set; }
 
         /// The real time in seconds since the game started (Read Only).
-        public static FixedPoint realtimeSinceStartup =>
+        public FixedPoint realtimeSinceStartup =>
             (DateTime.Now - _initTime).TotalSeconds;
 
-        public static FixedPoint realtimeSinceStartupMS =>
+        public FixedPoint realtimeSinceStartupMS =>
             (DateTime.Now - _initTime).TotalMilliseconds;
 
-        public static void Init() {
+        public void Init() {
             _initTime = DateTime.Now;
         }
 
-        public static void Update() {
+        public void Update() {
             var now = DateTime.Now;
             deltaTime = (now - lastFrameTime).TotalSeconds;
             timeSinceLevelLoad = (now - _initTime).TotalSeconds;
