534c31aeee1245deeb3c96271ab28dd12d8f87b3
luoguohui <luoguohui1986@gmail.com>
2019-11-13 16:07:52 +0800

    修改fixedpoint的显示

--- vFrame.Lockstep.Editor/Editor/EditorFixedPoint.cs
+++ vFrame.Lockstep.Editor/Editor/EditorFixedPoint.cs
@@ -5,20 +5,21 @@ using UnityEditor;
 namespace vFrame.Lockstep.Core.Editor
 {
     [CustomPropertyDrawer(typeof(FixedPoint))]
-    public class EditorLFloat : UnityEditor.PropertyDrawer
+    public class EditorFixedPoint : UnityEditor.PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
             var xProperty = property.FindPropertyRelative("_serializedValue");
 
-            float LabelWidth = EditorGUIUtility.labelWidth - EditorTSVectorDrawTool.LableWidthOffset;
-            float lableWid = EditorTSVectorDrawTool.LableWid;
-            var labelRect = new Rect(position.x, position.y, LabelWidth, position.height);
+            float labelWidth = EditorGUIUtility.labelWidth - EditorTSVectorDrawTool.LableWidthOffset;
+            var labelRect = new Rect(position.x, position.y, labelWidth, position.height);
             EditorGUI.LabelField(labelRect, label);
 
-            float filedWid = (position.width - LabelWidth);
-            float initX = position.x + LabelWidth;
+            float filedWid = (position.width - labelWidth);
+            float initX = position.x + labelWidth;
             var valRect = new Rect(initX, position.y, filedWid, position.height);
-            EditorGUI.FloatField(valRect, xProperty.longValue);
+
+            var fVal = EditorGUI.FloatField(valRect, xProperty.longValue * 1.0f / FixedPoint.L_ONE);
+            xProperty.longValue = (long) (fVal * FixedPoint.L_ONE);
         }
     }
 }
--- vFrame.Lockstep.Editor/Editor/EditorTSVectorDrawTool.cs
+++ vFrame.Lockstep.Editor/Editor/EditorTSVectorDrawTool.cs
@@ -11,12 +11,13 @@ namespace vFrame.Lockstep.Core.Editor
 
         public static void DrawField(Rect position, float initX, ref float offset, float lableWid, float filedWid,
             SerializedProperty property, GUIContent label) {
-            var lableRect = new Rect(initX + offset, position.y, 70, position.height);
-            EditorGUI.LabelField(lableRect, label.text);
+            var labelRect = new Rect(initX + offset, position.y, 70, position.height);
+            EditorGUI.LabelField(labelRect, label.text);
+
             var valRect = new Rect(initX + offset + lableWid, position.y, filedWid, position.height);
-//        var fVal = EditorGUI.FloatField(valRect, property.intValue * 1.0f / LFloat.Precision);
-//        property.intValue = (int) (fVal * LFloat.Precision);
-            EditorGUI.FloatField(valRect, property.longValue);
+            var fVal = EditorGUI.FloatField(valRect, property.longValue * 1.0f / FixedPoint.L_ONE);
+            property.longValue = (long) (fVal * FixedPoint.L_ONE);
+
             offset += filedWid + lableWid;
         }
     }
