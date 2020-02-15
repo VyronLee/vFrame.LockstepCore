62db5b698a6b335ead11fbcaaba5e53db05947f4
vyronlee <lwz_jz@hotmail.com>
2020-02-15 17:35:30 +0800

    调整dll依赖

--- BuildAllPlatform.sh
+++ BuildAllPlatform.sh
@@ -3,9 +3,11 @@
 CUR_DIR=$(pwd)
 
 echo "Building for platform: Editor"
-msbuild vFrame.Lockstep.Core.sln /t:Clean,Rebuild /p:Configuration=Debug /p:Platform="Any CPU" /p:DefineConstants="TRACE DEBUG UNITY_EDITOR"
+msbuild vFrame.Lockstep.Core.sln /t:Clean,Rebuild /p:Configuration=Release /p:Platform="Any CPU" /p:DefineConstants="TRACE"
+mkdir -p ${CUR_DIR}/Output/vFrame.Lockstep.Core/Editor
 mkdir -p ${CUR_DIR}/Output/vFrame.Lockstep.Core/Runtime
-cp -f ${CUR_DIR}/Build/vFrame.Lockstep.Core/Debug/vFrame.Lockstep.Core.* ${CUR_DIR}/Output/vFrame.Lockstep.Core/Runtime/
+cp -f ${CUR_DIR}/Build/vFrame.Lockstep.Core/Release/vFrame.Lockstep.Core.* ${CUR_DIR}/Output/vFrame.Lockstep.Core/Runtime/
+cp -f ${CUR_DIR}/Build/vFrame.Lockstep.Core/Release/vFrame.Lockstep.Editor.* ${CUR_DIR}/Output/vFrame.Lockstep.Core/Editor/
 
 echo "Building for platform: Standalone"
 msbuild vFrame.Lockstep.Core.sln /t:Clean,Rebuild /p:Configuration=Release /p:Platform="Any CPU" /p:DefineConstants="TRACE UNITY_STANDALONE"
